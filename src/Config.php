8b21173390ec0ab54cfd5be0752928b85bfb0754
朱正钱 <hachi.zzq@gmail.com>
2018-11-05 15:45:25 +0800

    init framework

---
+++ src/Config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * DateTime: 2018/11/5 15:39
+ * @author: hachi.zzq <hachi.zzq@gmail.com>
+ */
+
+namespace Hachi\LaravelMutiTenancy;
+
+
+class Config
+{
+
+}
---
+++ src/Environment.php
@@ -0,0 +1,18 @@
+<?php namespace Hachi\LaravelMutiTenancy;
+
+use Illuminate\Contracts\Foundation\Application;
+/**
+ * Created by PhpStorm.
+ * DateTime: 2018/11/5 15:37
+ * @author: hachi.zzq <hachi.zzq@gmail.com>
+ */
+
+class Environment
+{
+
+    public function __construct(Application $app)
+    {
+
+    }
+
+}
